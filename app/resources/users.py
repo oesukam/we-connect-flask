8e3a3a706e6edcfd4b72a5576650b2360ba90000
Olivier ESUKA MUSELEMU <oesukam@gmail.com>
2019-03-03 15:24:42 +0200

    feat(auth): implement login

--- app/resources/users.py
+++ app/resources/users.py
@@ -3,11 +3,11 @@ from flask_restful import Resource, reqparse
 from flask_jwt_extended import create_access_token
 from app.models.user_model import UserModel
 from app.helpers.validator import Validate
-parser = reqparse.RequestParser()
 
 
 class Signup(Resource, UserModel):
     def post(self):
+        parser = reqparse.RequestParser()
         parser.add_argument('email', required=True, type=str,
                             help=Validate.message('required', 'Email'))
         parser.add_argument(
@@ -21,13 +21,13 @@ class Signup(Resource, UserModel):
                 "message": "Wrong email format"
             }, 400
 
-        if len(args.get('username')) < 5:
+        if not args.get('username') or len(args.get('username')) < 5:
             return {
                 "status": "FAILED",
                 "message": "Username should have minimum 6 characters"
             }, 400
 
-        if len(args.get('password')) < 5:
+        if not args.get('password') or len(args.get('password')) < 5:
             return {
                 "status": "FAILED",
                 "message": "password should have minimum 6 characters"
@@ -52,3 +52,44 @@ class Signup(Resource, UserModel):
             "token": access_token,
             "message": "Signup successfully"
         }, 201
+
+
+class Login(Resource, UserModel):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument(
+            'username', required=True, type=str, help=Validate.message('required', 'Username'))
+        parser.add_argument(
+            'password', required=True, type=str, help=Validate.message('required', 'Password'))
+        args = parser.parse_args()
+
+        if args.get('username') and len(args.get('username')) < 5:
+            return {
+                "status": "FAILED",
+                "message": "Username should have minimum 6 characters"
+            }, 400
+
+        if not args.get('password') or len(args.get('password')) < 5:
+            return {
+                "status": "FAILED",
+                "message": "password should have minimum 6 characters"
+            }, 400
+
+        user = UserModel.find_by_username(args.get('username'))
+        if not user:
+            return {
+                "status": "FAILED",
+                "message": "Username and password don't match"
+            }, 401
+
+        access_token = create_access_token(identity=user['username'])
+
+        return {
+            "status": "SUCCESS",
+            "user": {
+                "username": args.get('username'),
+                "email": user["email"],
+                "id": user["id"],
+            },
+            "token": access_token
+        }, 200
